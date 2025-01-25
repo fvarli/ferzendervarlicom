3ddaf20d1b10505f373e474cfa22490d3d861978
Ferzender Varli <fvarlii@gmail.com>
2025-01-26 00:38:26 +0300

    updated for seo

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,12 @@ const menuButton = document.getElementById('menuButton');
 const mobileMenu = document.getElementById('mobileMenu');
 const closeMenu = document.getElementById('closeMenu');
 const links = document.querySelectorAll('#navLinks li a');
+const validHashes = Array.from(links)
+    .map(link => link.getAttribute('href'))
+    .filter(href => href.startsWith('#'))
+    .filter((value, index, self) => self.indexOf(value) === index);
 
+// Toggle mobile menu visibility
 menuButton.addEventListener('click', () => {
     mobileMenu.classList.remove('hidden');
 });
@@ -11,7 +16,7 @@ closeMenu.addEventListener('click', () => {
     mobileMenu.classList.add('hidden');
 });
 
-// Add event listener to each link to close the menu when clicked
+// Add event listener to each link to close the menu when clicked and set active link
 links.forEach(link => {
     link.addEventListener('click', () => {
         mobileMenu.classList.add('hidden');  // Close the menu when any link is clicked
@@ -20,21 +25,7 @@ links.forEach(link => {
     });
 });
 
-// Set active link on page load
-window.addEventListener('load', () => {
-    const currentHash = window.location.hash || "#home"; // Get current hash or default to #home
-    links.forEach(link => {
-        if (link.getAttribute('href') === currentHash) {
-            link.classList.add('bg-gray-500'); // Add active class
-        } else {
-            link.classList.remove('bg-gray-500'); // Remove active class
-        }
-    });
-
-    handleHashChange(); // Handle tab visibility on page load
-});
-
-// Function to handle tab switching
+// Function to handle tab switching and invalid hash handling
 function handleHashChange() {
     const hash = window.location.hash || "#home"; // Default to #home
     const tabs = document.querySelectorAll(".tab-content");
@@ -46,10 +37,29 @@ function handleHashChange() {
             tab.classList.remove("hidden");
         }
     });
+
+    // Handle invalid hash by redirecting to #home
+    if (!validHashes.includes(hash)) {
+        window.location.hash = "#home"; // Redirect to valid hash
+    }
+
+    // Set active link based on hash
+    links.forEach(link => {
+        if (link.getAttribute('href') === hash) {
+            link.classList.add('bg-gray-500'); // Add active class
+        } else {
+            link.classList.remove('bg-gray-500'); // Remove active class
+        }
+    });
 }
 
+// Listen for hash changes and handle accordingly
 window.addEventListener('hashchange', handleHashChange);
 
+// Set active link on page load
+window.addEventListener('load', handleHashChange);
+
+// Set email elements dynamically
 const user = "contact";
 const domain = "ferzendervarli.com";
 const emailElement = document.getElementById("contactEmail");
