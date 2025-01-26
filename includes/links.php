1c1d479020cacad98a219674cea25e95312d45c8
Ferzender Varli <fvarlii@gmail.com>
2025-01-26 03:29:25 +0300

    updated social links

--- includes/links.php
+++ includes/links.php
@@ -13,7 +13,7 @@
 
     <!-- Social Media Links -->
     <div class="p-4 border-t border-gray-700">
-        <div class="flex justify-center space-x-6 text-gray-300">
+        <div class="flex justify-center justify-between text-gray-300">
             <a href="https://www.linkedin.com/in/fvarli/" target="_blank" aria-label="LinkedIn" title="LinkedIn" rel="noopener noreferrer" class="hover:text-gray-400">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
                     <path d="M9.237 8.855v12.139h3.769v-6.003c0-1.584.298-3.118 2.262-3.118 1.937 0 1.961 1.811 1.961 3.218v5.904H21v-6.657c0-3.27-.704-5.783-4.526-5.783-1.835 0-3.065 1.007-3.568 1.96h-.051v-1.66H9.237zm-6.142 0H6.87v12.139H3.095z"></path>
@@ -24,6 +24,9 @@
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
                     <path d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 012.496-.336 9.554 9.554 0 012.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z"></path>
                 </svg>
+            </a>
+            <a href="https://www.fiverr.com/f_varli" target="_blank" aria-label="Upwork" title="Fiverr" rel="noopener noreferrer" class="hover:text-gray-400" style="filter: invert(1)">
+            <svg fill="#000000" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin" class="jam jam-fiverr"><path d='M16.25 16.25v-10h-10v-.625c0-1.034.841-1.875 1.875-1.875H10V0H8.125A5.632 5.632 0 0 0 2.5 5.625v.625H0V10h2.5v6.25H0V20h8.75v-3.75h-2.5V10h6.285v6.25H10V20h8.75v-3.75h-2.5z'/><circle cx='14.375' cy='1.875' r='1.875'/></svg>
             </a>
             <a href="https://www.upwork.com/freelancers/~01f9931d59f0daa010?s=1110580755107926016" target="_blank" aria-label="Upwork" title="Upwork" rel="noopener noreferrer" class="hover:text-gray-400">
                 <img src="assets/images/upwork.svg" style="filter: invert(1)" alt="Upwork" class="w-6 h-6">
